89e38142025e867c8a26a17b21f2e03d81a2902f
nbezanis <nbezanis98@gmail.com>
2020-04-18 21:42:58 -0400

    add files for questionEditor component

---
+++ PollBuddy-Server/frontend/src/components/questionEditor/questionEditor.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import {MDBContainer} from "mdbreact";
+
+export default class QuestionEditor extends Component {
+    render() {
+        return(<MDBContainer/>);
+    }
+}
